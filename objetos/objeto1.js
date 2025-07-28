5e52d3096fd5e876422a636e49196f1171c3ad71
Vinisetim <viniciusgueimis@gmail.com>
2025-07-28 10:47:33 -0300

    Add files via upload

---
+++ objetos/objeto1.js
@@ -0,0 +1,7 @@
+let pessoa = {
+    nome: "Washington",
+    idade: 90,
+    curso: "Carpintaria",
+
+}
+console.log(`Este é o aluno ${pessoa.nome} da turma de ${pessoa.curso}. Ele possui ${pessoa.idade} anos.`);
---
+++ objetos/personagem.js
@@ -0,0 +1,8 @@
+let personagem = {
+    Nome: "Artoria",
+    Classe: "Cavaleiro",
+    pontosDEVida: 100,
+    falarFrase(){
+        console.log("Excalibaaaaaaaaaaaaa")
+}
+}
